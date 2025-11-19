2e8f5a202d37be28b02b29c9491d8c3c8c00f839
filde <dmtr.filimonov@yandex.ru>
2025-11-20 02:37:52 +0300

    Add fk

--- domains/models/pull_request.go
+++ domains/models/pull_request.go
@@ -3,11 +3,12 @@ package models
 import "time"
 
 type PullRequest struct {
-	PullRequestID     string `gorm:"primaryKey"`
-	PullRequestName   string `gorm:"not null"`
-	AuthorID          string
+	PullRequestID     string   `gorm:"primaryKey"`
+	PullRequestName   string   `gorm:"not null"`
+	AuthorID          string   `gorm:"not null"`
 	Status            string   `gorm:"not null"`
 	AssignedReviewers []string `gorm:"not null"`
 	CreatedAt         *time.Time
 	MergedAt          *time.Time
+	User              `gorm:"foreignKey:AuthorID;references:UserID" json:"-"`
 }
--- domains/models/user.go
+++ domains/models/user.go
@@ -3,6 +3,7 @@ package models
 type User struct {
 	UserID   string `gorm:"primaryKey"`
 	Username string `gorm:"not null"`
-	TeamName string
-	IsActive bool `gorm:"not null;default:true"`
+	TeamName string `gorm:"not null"`
+	IsActive bool   `gorm:"not null;default:true"`
+	Team     `gorm:"foreignKey:TeamName;references:TeamName" json:"-"`
 }
