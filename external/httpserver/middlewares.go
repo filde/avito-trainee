0c1f9a0007411a1c37f8064ad330c596a1197cbd
filde <dmtr.filimonov@yandex.ru>
2025-11-22 15:28:15 +0300

    Minor fix

--- external/httpserver/middlewares.go
+++ external/httpserver/middlewares.go
@@ -11,8 +11,8 @@ func (httpServer *HttpServer) accessControlMiddleware(next http.Handler) http.Ha
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, If-Match")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 			return
 		}
 		next.ServeHTTP(w, r)
