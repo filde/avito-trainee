b342ec86c38660bc1557f05a2c62d58752be488f
filde <dmtr.filimonov@yandex.ru>
2025-11-22 12:21:45 +0300

    Add errors text

--- domains/models/team.go
+++ domains/models/team.go
@@ -4,3 +4,7 @@ type Team struct {
 	TeamName string `gorm:"primaryKey" json:"team_name"`
 	Members  []User `gorm:"-" json:"members"`
 }
+
+type TeamResponse struct {
+	Team *Team `json:"team"`
+}
--- helpers/error_by_code.go
+++ helpers/error_by_code.go
@@ -1,13 +1,28 @@
 package helpers
 
 import (
+	"avito-trainee/common/constants"
 	"avito-trainee/domains/models"
+	"fmt"
 	"github.com/rs/zerolog/log"
 	"net/http"
 )
 
 func GetError(code string, optional ...string) *models.ErrorType {
-
+	codeError := &models.ErrorType{Code: code}
+	switch code {
+	case constants.BAD_BODY:
+		codeError.Message = constants.BAD_BODY_TEXT
+	case constants.USER_EXISTS:
+		codeError.Message = constants.USER_EXISTS_TEXT
+	case constants.TEAM_EXISTS:
+		if len(optional) != 1 {
+			codeError.Message = fmt.Sprintf(constants.TEAM_EXISTS_TEXT, "team")
+		} else {
+			codeError.Message = fmt.Sprintf(constants.TEAM_EXISTS_TEXT, optional[0])
+		}
+	}
+	return codeError
 }
 
 func WriteResponse(w http.ResponseWriter, response []byte) bool {
