78e9e1d8df0f84fd011d5a7445a32fc3e3085a98
filde <dmtr.filimonov@yandex.ru>
2025-11-23 17:04:18 +0300

    Minor fix

--- domains/models/user.go
+++ domains/models/user.go
@@ -1,12 +1,11 @@
 package models
 
 type User struct {
-	UserID       string `gorm:"primaryKey" json:"user_id"`
-	Username     string `gorm:"not null" json:"username"`
-	TeamName     string `gorm:"not null" json:"-"`
-	IsActive     bool   `gorm:"not null" json:"is_active"`
-	Team         `gorm:"foreignKey:TeamName;references:TeamName" json:"-"`
-	PullRequests []*PullRequest `gorm:"-" json:"-"`
+	UserID   string `gorm:"primaryKey" json:"user_id"`
+	Username string `gorm:"not null" json:"username"`
+	TeamName string `gorm:"not null" json:"-"`
+	IsActive bool   `gorm:"not null" json:"is_active"`
+	Team     `gorm:"foreignKey:TeamName;references:TeamName" json:"-"`
 }
 
 type UserResponse struct {
